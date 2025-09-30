e851d093d5ae361280f092faa296e86c043dabfd
damon <damonfedorick@gmail.com>
2025-09-30 06:58:22 -0700

    Enjoy

--- client.c
+++ client.c
@@ -1,23 +1,64 @@
-
 #include <stdio.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
-#include <poll.h> 
+#include <poll.h>
 #include <unistd.h>
+#include <netdb.h>
+#include <string.h>
+#include <stdlib.h>
 
 #include "ip.h"
 
-int main() {
+int main(int argc, char *argv[]) {
+    char *test_msg = NULL;
+    int headless = 0;
 
-    int sockfd = socket(AF_INET6, SOCK_STREAM, 0);
+    // Parse command line arguments
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-m") == 0 && i + 1 < argc) {
+            test_msg = argv[i + 1];
+            headless = 1;
+            i++;
+        }
+    }
 
-    struct sockaddr_in6 address = {0};
-    address.sin6_family = AF_INET6;
-    address.sin6_port = htons(9999);
-    address.sin6_addr = in6addr_any;
+    struct addrinfo hints = {0};
+    struct addrinfo *result;
 
-    inet_pton(AF_INET6, IP, &address.sin6_addr);
-    connect(sockfd, (struct sockaddr*)&address, sizeof(address));
+    hints.ai_family = AF_UNSPEC; // Support both IPv4 and IPv6
+    hints.ai_socktype = SOCK_STREAM;
+
+    if (getaddrinfo(IP, "9999", &hints, &result) != 0) {
+        perror("getaddrinfo failed");
+        return 1;
+    }
+
+    int sockfd = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+    if (sockfd < 0) {
+        perror("socket failed");
+        freeaddrinfo(result);
+        return 1;
+    }
+
+    if (connect(sockfd, result->ai_addr, result->ai_addrlen) < 0) {
+        perror("connect failed");
+        close(sockfd);
+        freeaddrinfo(result);
+        return 1;
+    }
+
+    freeaddrinfo(result);
+
+    printf("Connected to server at %s:9999\n", IP);
+    fflush(stdout);
+
+    // If headless mode, send test message immediately
+    if (headless && test_msg) {
+        int len = strlen(test_msg);
+        send(sockfd, test_msg, len, 0);
+        printf("Sent: %s", test_msg);
+        fflush(stdout);
+    }
 
     // stdin == 0
     struct pollfd fds[2] = {
@@ -29,22 +70,41 @@ int main() {
         }
     };
 
+    int received_response = 0;
     for (;;) {
         char buffer[256] = {0};
-        poll(fds, 2, 50000);
+        int timeout = headless ? 5000 : 50000;
+        int ret = poll(fds, 2, timeout);
+
+        if (ret == 0 && headless && received_response) {
+            // Timeout in headless mode after receiving response
+            printf("Test complete\n");
+            break;
+        }
+
         if (fds[0].revents & POLLIN) {
-            read(0, buffer, 255);
-            send(sockfd, buffer, 255, 0);
+            int len = read(0, buffer, 255);
+            if (len > 0) {
+                send(sockfd, buffer, len, 0);
+            }
 
         }
         else if (fds[1].revents & POLLIN) {
-            if (recv(sockfd, buffer, 255, 0) == 0) {
+            int len = recv(sockfd, buffer, 255, 0);
+            if (len == 0) {
+                printf("Server disconnected\n");
                 return 0;
             }
-            printf("%s", buffer);
+            if (len > 0) {
+                printf("Received: ");
+                write(1, buffer, len);
+                fflush(stdout);
+                received_response = 1;
+            }
         }
     }
 
+    close(sockfd);
     return 0;
 }
 
--- server.c
+++ server.c
@@ -4,20 +4,50 @@
 #include <sys/socket.h>
 #include <poll.h>
 #include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
 
-int main() {
+int main(int argc, char *argv[]) {
+    char *test_msg = NULL;
+    int headless = 0;
 
-    int sockfd = socket(AF_INET6, SOCK_STREAM, 0);
+    // Parse command line arguments
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-m") == 0 && i + 1 < argc) {
+            test_msg = argv[i + 1];
+            headless = 1;
+            i++;
+        }
+    }
 
-    struct sockaddr_in6 address = {
-        AF_INET6,
-        htons(9999),
-        0
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    struct sockaddr_in address = {
+        .sin_family = AF_INET,
+        .sin_port = htons(9999),
+        .sin_addr.s_addr = INADDR_ANY
     };
 
-    bind(sockfd, (struct sockaddr*)&address, sizeof(address));
-    listen(sockfd, 10);
+    if (bind(sockfd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        return 1;
+    }
+
+    if (listen(sockfd, 10) < 0) {
+        perror("listen failed");
+        return 1;
+    }
+
+    printf("Server listening on 0.0.0.0:9999\n");
+    fflush(stdout);
+
     int clientfd = accept(sockfd, 0, 0);
+    if (clientfd < 0) {
+        perror("accept failed");
+        return 1;
+    }
+    printf("Client connected (fd=%d)\n", clientfd);
+    fflush(stdout);
 
     // stdin == 0
     struct pollfd fds[2] = {
@@ -29,22 +59,50 @@ int main() {
         }
     };
 
+    int received_response = 0;
     for (;;) {
         char buffer[256] = {0};
-        poll(fds, 2, 50000);
+        int timeout = headless ? 5000 : 50000;
+        int ret = poll(fds, 2, timeout);
+
+        if (ret == 0 && headless && received_response) {
+            // Timeout in headless mode after receiving response
+            printf("Test complete\n");
+            break;
+        }
+
         if (fds[0].revents & POLLIN) {
-            read(0, buffer, 255);
-            send(clientfd, buffer, 255, 0);
+            int len = read(0, buffer, 255);
+            if (len > 0) {
+                send(clientfd, buffer, len, 0);
+            }
 
         }
         else if (fds[1].revents & POLLIN) {
-            if (recv(clientfd, buffer, 255, 0) == 0) {
+            int len = recv(clientfd, buffer, 255, 0);
+            if (len == 0) {
+                printf("Client disconnected\n");
                 return 0;
             }
-            printf("%s", buffer);
+            if (len > 0) {
+                printf("Received: ");
+                write(1, buffer, len);
+                fflush(stdout);
+
+                // Echo back to client in headless mode
+                if (headless && test_msg) {
+                    int msg_len = strlen(test_msg);
+                    send(clientfd, test_msg, msg_len, 0);
+                    printf("Sent: %s", test_msg);
+                    fflush(stdout);
+                }
+                received_response = 1;
+            }
         }
     }
 
+    close(clientfd);
+    close(sockfd);
     return 0;
 }
 
