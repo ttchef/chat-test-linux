70453c6f00cecd7f808a1cad1e179a063c7c8dbd
ttchef <ruro0.edit@gmail.com>
2025-10-03 11:37:00 +0200

    refinied json structure

---
+++ lib/ws_json.c
@@ -0,0 +1,9 @@
+
+#include "ws_json.h"
+
+int32_t wsJsonAddString(wsJson *obj, const char *key, const char *val) {
+    
+
+    return WS_JSON_OK;
+}
+
--- lib/ws_json.h
+++ lib/ws_json.h
@@ -2,20 +2,39 @@
 #ifndef WS_JSON_H
 #define WS_JSON_H
 
+#define WS_JSON_ERROR -1 
+#define WS_JSON_OK 0
+
 #define WS_JSON_MAX_KEY_SIZE 64 
 #define WS_JSON_MAX_VALUE_SIZE 256
 #define WS_JSON_OBJECT_MAX_FIELDS 16
 
 #include <stdint.h>
+#include <stdlib.h>
 
-typedef struct {
+typedef enum {
+    WS_JSON_STRING,
+    WS_JSON_NUMBER,
+    WS_JSON_OBJECT,
+} wsJsonType;
+
+typedef struct wsJsonField {
     char key[WS_JSON_MAX_KEY_SIZE];
-    char value[WS_JSON_MAX_VALUE_SIZE];
-} wsJsonField;
+    wsJsonType type; 
+    union {
+        char stringValue[WS_JSON_MAX_VALUE_SIZE];
+        double numberValue;
+        struct {
+            struct wsJsonField* children[WS_JSON_OBJECT_MAX_FIELDS];
+            int32_t childCount;
+        } object;
+    };
+} wsJson;
 
-typedef struct {
-    wsJsonField fields[WS_JSON_OBJECT_MAX_FIELDS];
-    int32_t count;
-} wsJsonObject;
+int32_t wsJsonAddString(wsJson* obj, const char* key, const char* val);
+int32_t wsJsonAddNumber(wsJson* obj, const char* key, double val);
+int32_t wsJsonCreateChild(wsJson* child, const char* key);
+int32_t wsJsonAddChild(wsJson* obj, wsJson* child);
+int32_t wsJsonToString(wsJson* obj, char* out, size_t size);
 
 #endif
